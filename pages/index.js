720e426810b62f45457e2b4e830755799380540c
halisa <halisasaipulaa@gmail.com>
2023-03-10 10:42:37 -0800

    installed @heroicons and rendered on the header section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-import Avatar from '../components/Avatar'
-import Head from 'next/head'
+import Head from 'next/head';
+import { ViewGridIcon } from '@heroicons/react/solid';
+
+import Avatar from '../components/Avatar';
 
 export default function Home() {
     return (
@@ -21,6 +23,7 @@ export default function Home() {
                     <p className="link">Images</p>
 
                     {/* Icon */}
+                    <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer"/>
                     {/* Avatar */}
                     <Avatar url="https://static-exp1.licdn.com/sc/h/dxf91zhqd2z6b0bwg85ktm5s4"/>
                 </div>

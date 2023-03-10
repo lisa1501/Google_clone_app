cfce5d1c79e7fffb732e1b683c141832bbf8715a
halisa <halisasaipulaa@gmail.com>
2023-03-10 09:15:35 -0800

    Initialising the build and Tailwind CSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    mode: 'jit',
+    content: [
+    
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+
+    ],
+    darkMode:false,
+    theme: {
+        extend: {},
+    },
+    variants:{
+        extend:{},
+    },
+    plugins: [],
+}

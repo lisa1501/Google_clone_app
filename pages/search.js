8eda82a5a7f501dc1dac4b2345d8c5767573b3c0
halisa <halisasaipulaa@gmail.com>
2023-03-11 11:50:08 -0800

    created search page

---
+++ pages/search.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+function search() {
+    return (
+        <div>
+            <Head>
+                <title>Search Results</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            {/* Header */}
+            <h1>Search Page</h1>
+            {/* Search Results*/}
+        </div>
+    )
+}
+
+export default search;

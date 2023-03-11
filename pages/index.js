810aecd12e42914839c30686e26fcaffcc544dc4
halisa <halisasaipulaa@gmail.com>
2023-03-11 11:43:56 -0800

    build the Search functionality

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,25 @@ import Head from 'next/head';
 import { CameraIcon, MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid';
 import { SearchIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
-
+import {  useRef } from'react';
+import { useRouter } from 'next/router';
 
 import Avatar from '../components/Avatar';
 import Footer from '../components/Footer';
 
 export default function Home() {
+    const router = useRouter();
+    const searchInputRef = useRef(null);
+    const search = (e) =>{
+        e.preventDefault();
+        const term = searchInputRef.current.value;
+
+        if(!term) return ;
+
+        router.push(`/search?term=${term}`);
+
+    }
+
     return (
         <div className='flex flex-col items-center justify-center h-screen'>
         <Head>
@@ -44,7 +57,7 @@ export default function Home() {
                                 max-w-md rounded-full border border-gray-200 px-5 py-3 items-center 
                                 sm:max-w-xl lg:max-w-2xl'>
                     <SearchIcon className='h-5 mr-3 text-gray-500'/> 
-                    <input type="text" placeholder="Search Google or type a URL" className="flex-grow focus:outline-none"/>
+                    <input ref={searchInputRef} type="text" placeholder="Search Google or type a URL" className="flex-grow focus:outline-none"/>
                     <div className='flex space-x-2 items-center'>
                         <MicrophoneIcon className='h-5' />
                         <CameraIcon className='h-5' />
@@ -54,8 +67,8 @@ export default function Home() {
 
                 <div className='flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0
                                 sm:flex-row sm:space-x-4'>
-                    <button className='btn'>Google Search</button>
-                    <button className='btn'>I'm Feeling Lucky</button>
+                    <button onClick={search} className='btn'>Google Search</button>
+                    <button onClick={search} className='btn'>I'm Feeling Lucky</button>
                 </div>
                 
             </form>
